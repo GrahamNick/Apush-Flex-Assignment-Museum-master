87ea6014ab0b1f9032ee181e908fe5a1d47903ea
GrahamNick <nick.g.coac@gmail.com>
2019-03-11 11:37:55 -0400

    Initial commit

---
+++ Apush-Flex-Assignment-Museum-master/Assets/pictureNew.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pictureNew : MonoBehaviour {
+	public GameObject prefab;
+
+
+	void OnTriggerEnter(Collider other) {
+		Instantiate (prefab);
+	}
+	void OnTriggerExit(Collider other){
+		Destroy(prefab);
+	}
+}
